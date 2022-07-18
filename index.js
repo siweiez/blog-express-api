6be196a9aaac13d7a7001779defe221f92f02b87
siweiez <102887631+siweiez@users.noreply.github.com>
2022-07-17 18:10:50 -0700

    add cors

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+var cors = require('cors');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
@@ -10,12 +11,14 @@ const categoryRoute = require('./routes/categories');
 
 dotenv.config();
 app.use(express.json());
+app.use(cors({origin: `*`}));
 
 mongoose.connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
 app.use('/', express.static('build'));
+
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
