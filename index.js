aa9aa84a40effb5aa30e038db73694ba74862ead
siweiez <102887631+siweiez@users.noreply.github.com>
2022-07-17 17:05:34 -0700

    .

--- index.js
+++ index.js
@@ -15,10 +15,15 @@ mongoose.connect(process.env.MONGO_URL)
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use('/', express.static('build'));
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 app.use('/api/categories', categoryRoute);
+
+app.get("/", (request, response) => {
+  response.json({ message: "Server is Running" });
+});
 
 app.listen(process.env.PORT || "5000", () => {
   console.log("--------------Application is running--------------");
